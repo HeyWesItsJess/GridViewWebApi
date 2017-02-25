41ddedd28f55900fc4af84f491813f7621291680
Jesse Truett Hufstetler <Jesse.Hufstetler+githubWesStuff@gmail.com>
2017-02-25 18:11:22 -0500

    Template

---
+++ GridViewWebApi/GridViewWebApi/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GridViewWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ GridViewWebApi/GridViewWebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GridViewWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
